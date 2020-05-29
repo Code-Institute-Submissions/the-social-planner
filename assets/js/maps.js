8fb0ffae0e459bc95448fd6485f7ad1e9c5a8eff
Ryan Anderson <RJAnderson94@gmail.com>
2020-05-29 15:55:00 +0000

    contact form template added, footer created with social icons

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,6 +2,5 @@ function initMap() {
   var london = {lat: 51.5127484, lng: -0.2052297};
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: london});
-  var marker = new google.maps.Marker({position: london, map: map});
 }
 // 51.5127484,-0.2052297
