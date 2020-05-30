33065980161651ec0bdfd3bb163ae5c798d458d2
Ryan Anderson <RJAnderson94@gmail.com>
2020-05-30 16:31:39 +0000

    cities added in array

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,29 @@
 // ----------------- Dropdown Menus
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+var countries = ['Austria','Belgium','Croatia','Denmark','France','Germany','Greece','Ireland','Spain','Sweden','United Kingdom'],
+    select = document.getElementById('countrySelect');
+for( country in countries ) {
+    select.add( new Option( countries[country] ) );
+};
 
-
-
-
+$("#countrySelect".value).click(function findCity(){
+    var select = document.getElementById('countrySelect');
+    var list2 = document.getElementById("citySelect");
+    var list1SelectedValue = list1.options[list1.selectedIndex].value;
+            if (list1SelectedValue=='Austria')
+            {
+            list2.options.length=0;
+            list2.options[0] = new Option('select a city', '');
+            list2.options[1] = new Option('Salzburg', 'Salzburg');
+            list2.options[2] = new Option('Vienna', 'Vienna');
+            }
+            else if (list1SelectedValue=='Belgium')
+            {  
+            list2.options.length=0;
+            list2.options[0] = new Option('select a city', '');
+            list2.options[1] = new Option('Brussels', 'Brussels');
+            list2.options[2] = new Option('Gent', 'Gent');
+            }
+})
 
 // ---------------- Map
 function initMap() {
