a7a1216b42e57750b9e3c611555ef9c621ad32eb
Ryan Anderson <RJAnderson94@gmail.com>
2020-05-31 08:48:53 +0000

    countries added to dropdown menu

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,1 +1,15 @@
-// ----------------- Dropdown Menus
+// ----------------- Dropdown Menus - codebyamir
+let countryDropdown = $('#country-dropdown');
+
+countryDropdown.empty();
+
+countryDropdown.append('<option selected="true" disabled>select a country</option>');
+countryDropdown.prop('selectedIndex', 0);
+
+const countries = 'assets/json/countries.json';
+
+$.getJSON(countries, function (data) {
+  $.each(data, function (key, entry) {
+    countryDropdown.append($('<option></option>').attr('value', entry.id).text(entry.name));
+  })
+});
