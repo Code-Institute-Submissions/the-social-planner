07c2bfd91c269d4fcd67f910bb5cbb5ee04eb5a8
Ryan Anderson <RJAnderson94@gmail.com>
2020-05-31 11:17:02 +0000

    dropdown format changed, amended code in dropdown.js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,15 +1,35 @@
-// ----------------- Dropdown Menus - codebyamir
-let countryDropdown = $('#country-dropdown');
+// ----------------- Country Dropdown Menu - codebyamir
+var countryDropdown = $("#country-dropdown");
 
 countryDropdown.empty();
 
 countryDropdown.append('<option selected="true" disabled>select a country</option>');
-countryDropdown.prop('selectedIndex', 0);
+countryDropdown.prop("selectedIndex", 0);
 
-const countries = 'assets/json/countries.json';
+const countries = "assets/json/countries.json";
 
 $.getJSON(countries, function (data) {
   $.each(data, function (key, entry) {
-    countryDropdown.append($('<option></option>').attr('value', entry.id).text(entry.name));
-  })
+    countryDropdown.append(
+      $("<option></option>").attr("value", entry.id).text(entry.name)
+    );
+  });
 });
+
+// ------------------- City Dropdown Menu
+
+var cityDropdown = $("#city-dropdown");
+const cities = ('assets/json/cities.json');
+
+cityDropdown.append(`<option selected="true" disabled>select a city</option>`);
+
+countryDropdown.on("change", function(event){
+    $.getJSON(cities, function(data){
+        cityDropdown.empty();
+        cityDropdown.append('<option selected="true" disabled>select a city</option>');
+        $.each(data[cityDropdown.val()], function(key, entry){
+            cityDropdown.append($('<option></option>').attr('value', entry.name).text(entry.name))
+        })
+    })
+})
+
