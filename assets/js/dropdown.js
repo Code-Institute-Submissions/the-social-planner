1157b84a4feba5c47744ade610c07ec0ac0a5c5d
Ryan Anderson <RJAnderson94@gmail.com>
2020-06-01 14:56:17 +0000

    geonames api added to get all countries

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,5 +1,5 @@
 // ----------------- Country Dropdown Menu - codebyamir
-let currentCities;
+/*let currentCities;
 var countryDropdown = $("#country-dropdown");
 
 countryDropdown.empty();
@@ -37,4 +37,52 @@ countryDropdown.on("change", function(city) {
             }
         }
     })
+});*/
+async function getData(geonameId) {
+    let response = await fetch(`//www.geonames.org/childrenJSON?geonameId=${geonameId}&style=long`);
+    return response.json();
+}
+$('#country-dropdown').on("change", async function() {
+    let $select = $('#city-dropdown'),
+        geonameId = $(this).val();
+        $select.empty();
+    const places = await getData(geonameId);
+    await places.geonames.map(({geonameId, name, lat, lng}) => {
+        let op;
+        if ($select.data('value') === geonameId && Ctrl.toLowerCase() === 'state') {
+            op = `<option value="${geonameId}" selected="selected">${name}</option>`;
+            getPlaces(geonameId, 'region');
+        } else if ($select.data('value') === geonameId && Ctrl.toLowerCase() === 'region') {
+            op = `<option value="${geonameId}" selected="selected">${name}</option>`;
+        } else {
+            op = `<option value="${geonameId}" data-lng="${lng}" data-lat="${lat}">${name}</option>`;
+        }
+        $select.append(op);
+    });
 });
+async function getCountriesInContinents(geonameId)
+{
+    var $select = $('#country-dropdown');
+    const continents = await getData(geonameId);
+    await continents.geonames.map(async ({geonameId, name}) => {
+        let countries = await getData(geonameId);
+        let $optgroup = $(`<optgroup label="${name}"/>`);
+        countries.geonames.map(({geonameId, name}) => {
+            let op;
+            if ($select.data('value') === geonameId) {
+                op = `<option value="${geonameId}" selected="selected">${name}</option>`;
+                getPlaces(geonameId, 'state');
+            } else {
+                op = `<option value="${geonameId}">${name}</option>`;
+            }
+            $optgroup.append(op);
+        });
+        $select.append($optgroup);
+    });
+};
+getCountriesInContinents('6295630');
+
+
+
+
+
